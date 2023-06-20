05cf50947afed58c10e2bcbad14d2feb7f0131af
thekolesnikov <thesharkov@gmail.com>
2023-06-20 11:28:33 +0300

    setup project

---
+++ src/components/CommentList/CommentList.tsx
@@ -0,0 +1,12 @@
+import CommentItem from '../CommentItem/CommentItem';
+
+function CommentList() {
+    return (
+        <div>
+            <CommentItem />
+            <CommentItem />
+        </div>
+    );
+}
+
+export default CommentList;
---
+++ src/components/InputForm/InputForm.tsx
@@ -0,0 +1,5 @@
+function InputForm() {
+    return <div>InputForm</div>;
+}
+
+export default InputForm;
